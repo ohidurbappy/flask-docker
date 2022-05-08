077a8cc6b87852692fef6eddb27dda7b744dc2c7
ohidurbappy <ohidurbappy@gmail.com>
2022-05-08 14:30:52 +0600

    update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello from Flask & Docker</h2>'
+    return '<h2>Hello from Flask & Docker.</h2>'+'<h3>Application running on port'+request.host+'</h3>'
 
 
 if __name__ == "__main__":
