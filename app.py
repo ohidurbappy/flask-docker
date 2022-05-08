118c6702f441ce9a46ff5411aed6a50f094840f5
ohidurbappy <ohidurbappy@gmail.com>
2022-05-08 15:04:11 +0600

    updated

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h2>Hello from Flask & Docker.</h2>'+'<h3>Application running on port '+request.host+'</h3>'
+    return '<h2>Hello from Flask & Docker.</h2>'+'<h3>Application running on port '+request.host+'</h3>'+\
+              f"<h3>This is Instance: {os.environ.get('INSTANCE_ID')}</h3>"
 
 
 if __name__ == "__main__":
