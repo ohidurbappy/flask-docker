8ac46c8952b51e0775c4220a6483674b440b1576
ohidurbappy <ohidurbappy@gmail.com>
2022-05-08 14:40:41 +0600

    fix typo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h2>Hello from Flask & Docker.</h2>'+'<h3>Application running on port'+request.host+'</h3>'
+    return '<h2>Hello from Flask & Docker.</h2>'+'<h3>Application running on port '+request.host+'</h3>'
 
 
 if __name__ == "__main__":
